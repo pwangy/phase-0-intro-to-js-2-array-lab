f6ecdcea48072d2a2789605629a155ccdecd996c
pwangy <blipsandclicks@gmail.com>
2024-01-07 14:00:19 -0800

    tests for destructive array methods pass

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+
+}
+
+function prependCat(name){
+
+}
+
+function removeLastCat(){
+
+}
+
+function removeFirstCat(){
+
+}
