b7ca24d921737b5387b65d09e938288a419f3f2d
pwangy <blipsandclicks@gmail.com>
2024-01-07 14:30:51 -0800

    all tests pass

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ function destructivelyRemoveFirstCat(){
 }
 
 function appendCat(name){
-
+    const newCats = [...cats, 'Broom']
+    return newCats
 }
 
 function prependCat(name){
-
+    const newCats = ['Arnold', ...cats]
+    return newCats
 }
 
 function removeLastCat(){
-
+    const newCats = cats.slice(0, 2)
+    return newCats
 }
 
 function removeFirstCat(){
-
+    const newCats = cats.slice(1)
+    return newCats
 }
