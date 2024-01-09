8860dcbeb14ec70be985d15f32b9c510706d7c82
pwangy <blipsandclicks@gmail.com>
2024-01-09 11:55:47 -0800

    solution check

--- index.js
+++ index.js
@@ -20,18 +20,30 @@ function appendCat(name){
     const newCats = [...cats, 'Broom']
     return newCats
 }
+// function appendCat(name) {
+//     return [...cats, name]
+// }
 
 function prependCat(name){
     const newCats = ['Arnold', ...cats]
     return newCats
 }
+// function prependCat(name) {
+//     return [name, ...cats]
+// }
 
 function removeLastCat(){
     const newCats = cats.slice(0, 2)
     return newCats
 }
+// function removeLastCat() {
+//     return cats.slice.(1)
+// }
 
 function removeFirstCat(){
     const newCats = cats.slice(1)
     return newCats
 }
+// function removeLastCat() {
+//     return cats.slice(0, cats.length -1)
+// }
